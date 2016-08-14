489680cb73d0c33ce0a561874216aceb1f51997e
keik <k4t0.kei@gmail.com>
2016-08-14 16:07:07 +0900

    Add classifiers to setup.py

--- sandbox_python_structure/__init__.py
+++ sandbox_python_structure/__init__.py
@@ -10,5 +10,5 @@ __desc__ = """Sandbox for Python project structure"""
 __author__ = 'keik'
 __author_email__ = 'k4t0.kei@gmail.com'
 __url__ = 'https://github.com/keik/sandbox-python-structure.git'
-__version__ = '0.0.1-dev'
+__version__ = '0.0.1-dev1'
 __license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -10,4 +10,14 @@ setup(name='sandbox-python-structure',
       author=sandbox_python_structure.__author__,
       author_email=sandbox_python_structure.__author_email__,
       url=sandbox_python_structure.__url__,
-      packages=find_packages())
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5'
+      ])
